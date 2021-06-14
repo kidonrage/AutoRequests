13545712cf71477c30606e829321f2f1a2e8d763
Vlad Eliseev <freshymanchan@gmail.com>
2021-06-14 22:05:01 +1000

    (feat/auth): Реализовал репозиторий пользовательской сессии

--- AutoRequestsKit/DataLayer/Repositories/Remote/Auth/AuthRemoteAPI.swift
+++ AutoRequestsKit/DataLayer/Repositories/Remote/Auth/AuthRemoteAPI.swift
@@ -1,5 +1,5 @@
 //
-//  UserSession.swift
+//  AuthRemoteAPI.swift
 //  AutoRequestsKit
 //
 //  Created by Vlad Eliseev on 14.06.2021.
@@ -8,10 +8,8 @@
 import Foundation
 import PromiseKit
 
-public protocol UserSessionRepository {
+public protocol AuthRemoteAPI {
 
-    func readUserSession() -> Promise<UserSession?>
     func signIn(login: String, password: String) -> Promise<UserSession>
-    func signOut(userSession: UserSession) -> Promise<UserSession>
 
 }
---
+++ AutoRequestsKit/DataLayer/Repositories/Remote/Auth/Implementations/FakeAuthRemoteAPI.swift
@@ -0,0 +1,54 @@
+//
+//  FakeAuthRemoteAPI.swift
+//  AutoRequestsKit
+//
+//  Created by Vlad Eliseev on 14.06.2021.
+//
+
+import Foundation
+import PromiseKit
+
+class FakeAuthRemoteAPI: AuthRemoteAPI {
+
+    // MARK: - Public Methods
+    func signIn(login: String, password: String) -> Promise<UserSession> {
+        let isDriver = login == "driver" && password == "driver"
+        let isPassenger = login == "passenger" && password == "passenger"
+
+        if isPassenger {
+            return signInAsPassenger()
+        } else if isDriver {
+            return signInAsDriver()
+        } else {
+            return Promise(error: NSError(domain: "test.auth", code: 0, userInfo: nil))
+        }
+    }
+
+    // MARK: - Private Methods
+    private func signInAsDriver() -> Promise<UserSession> {
+        return Promise<UserSession> { seal in
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                let profile = UserProfile(id: "driver",
+                                          name: "Driver Driverov",
+                                          mobileNumber: "+9 (999) 99-99-99")
+              let remoteUserSession = RemoteUserSession(token: "000000")
+              let userSession = UserSession(profile: profile, remoteSession: remoteUserSession)
+              seal.fulfill(userSession)
+            }
+        }
+    }
+
+    private func signInAsPassenger() -> Promise<UserSession> {
+        return Promise<UserSession> { seal in
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                let profile = UserProfile(id: "passenger",
+                                          name: "Passenger Passengerov",
+                                          mobileNumber: "+9 (999) 99-99-99")
+              let remoteUserSession = RemoteUserSession(token: "111111")
+              let userSession = UserSession(profile: profile, remoteSession: remoteUserSession)
+              seal.fulfill(userSession)
+            }
+        }
+    }
+
+}
